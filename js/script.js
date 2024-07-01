e0e6da5cb8a6dbcd84a5f13d6a452fef2bdec892
Krappiekop <bartijnmeurs@gmail.com>
2024-07-01 21:49:23 +0200

    Added dynamic sections and new pages for evenementen and garageverhuur

--- js/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Bedankt voor je bericht. We nemen spoedig contact met je op.');
-});
